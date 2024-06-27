3868b6395f35e8a886f68618ec172ba47b94c267
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-28 01:42:13 +0530

    implimented multiple animations and a gesture to the cards

--- InteractiveX/ContentView.swift
+++ InteractiveX/ContentView.swift
@@ -8,56 +8,174 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var showCard = false
+    
+
     var body: some View {
         
         ZStack {
             
-            VStack {
-                Spacer()
-            }
-            .frame(width: 300, height: 220)
-            .background(Color.blue)
+            TitleView()
+                .blur(radius:show ? 20 : 0)
+                .animation(.default)
+                
+            
+            
+            
+            BackCardView()
+            .background(show ? Color("card3") : Color("card4"))
             .cornerRadius(20)
             .shadow(radius: 20)
-            .offset(x: 0, y: -20)
+            .offset(x: 0, y: show ? -400 : -40)
+            .offset(x: viewState.width, y: viewState.height)
+            .scaleEffect(0.9)
+            .rotationEffect(.degrees(show ? 0 : 10))
+            .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+            .blendMode(.hardLight)
+            .animation(.easeIn(duration: 0.5))
             
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("UI design")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.semibold)
-                            .foregroundColor(Color.white)
-                        
-                        Text("Certificate")
-                            .foregroundStyle(Color("accent"))
-                    }
-                    .padding(.top, 20.0)
-                    Spacer()
-                    Image("Logo1")
-                }
-                .padding(.horizontal, 20.0)
-                .padding(/*@START_MENU_TOKEN@*/.top, 20.0/*@END_MENU_TOKEN@*/)
-                Spacer()
-                Image("Card1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 300 , height: 110, alignment: .top)
-                    .padding(40.0)
-                    
-                    
-            }
-            .frame(width: 340.0, height: 220.0)
-            .background(Color.black)
+            
+            BackCardView()
+            .background(show ? Color("card4") : Color("card3"))
             .cornerRadius(20)
-        .shadow(radius: 20)
-        }
-        
+            .shadow(radius: 20)
+            .offset(x: 0, y: show ? -200 : -20)
+            .offset(x: viewState.width, y: viewState.height)
+            .scaleEffect(0.95)
+            .rotationEffect(.degrees(show ? 0 : 5))
+            .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+            .blendMode(.hardLight)
+            .animation(.easeIn(duration: 0.3))
             
-        
+            
+            CardView()
+                .offset(x: viewState.width, y: viewState.height)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showCard.toggle()
+                }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                        self.show = true
+                        
+                    }
+                    
+                        .onEnded{ value in
+                            self.viewState = .zero
+                            self.show = false
+                            
+                        }
+                )
+                
+            
+            
+            BottomCardView()
+                .offset(x: 0, y: showCard ? 360 : 1000)
+                .blur(radius: show ? 20 : 0)
+            
+//            Used the cubic-bezier website to generate my custom animation timingCurve
+            
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+
+
+
+//MARK: - ExtractedViews
+
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI design")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    
+                    Text("Certificate")
+                        .foregroundStyle(Color("accent"))
+                }
+                .padding(.top, 20.0)
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20.0)
+            .padding(.top, 20.0)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300 , height: 110, alignment: .top)
+                .padding(40.0)
+            
+            
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220)
+        
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(5)
+                .opacity(0.1)
+            Text("This certificate is proof that Yashwant has achieved the UI design using swiftUI")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8.0)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        
+    }
 }
