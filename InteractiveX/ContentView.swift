858bca345e992b3f7b49f45aaec9f203b46a3272
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-27 21:02:13 +0530

    Initial Commit

---
+++ InteractiveX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveX
+//
+//  Created by Yashwant Sheshkar on 27/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveX/InteractiveXApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveXApp.swift
+//  InteractiveX
+//
+//  Created by Yashwant Sheshkar on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
