306d7ff99e73cd52081bf8ee1c7ef08946d6eedd
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-28 02:35:39 +0530

    Cerated the bottom pan gesture control using conditional statement.

--- InteractiveX/ContentView.swift
+++ InteractiveX/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
     @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+    
+    
     
 
     var body: some View {
@@ -20,39 +24,62 @@ struct ContentView: View {
             
             TitleView()
                 .blur(radius:show ? 20 : 0)
-                .animation(.default)
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(
+                    Animation
+                        .default
+                        .delay(0.1)
+//                        .speed(2)
+//                        .repeatCount(3, autoreverses: false)                
+                )
                 
             
             
             
             BackCardView()
+            .frame(width:showCard ? 300 : 340, height: 220)
             .background(show ? Color("card3") : Color("card4"))
             .cornerRadius(20)
             .shadow(radius: 20)
             .offset(x: 0, y: show ? -400 : -40)
             .offset(x: viewState.width, y: viewState.height)
-            .scaleEffect(0.9)
+            .offset(y : showCard ? -180 : 0)
+            .scaleEffect(showCard ? 1 : 0.9)
             .rotationEffect(.degrees(show ? 0 : 10))
-            .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+            .rotationEffect(Angle(degrees: showCard ? -10: 0))
+            .rotation3DEffect(Angle(degrees: showCard ? 0 : 10), axis: (x: 10.0, y: 0, z: 0))
             .blendMode(.hardLight)
             .animation(.easeIn(duration: 0.5))
             
             
+            
             BackCardView()
+            .frame(width: 340, height: 220)
             .background(show ? Color("card4") : Color("card3"))
             .cornerRadius(20)
             .shadow(radius: 20)
             .offset(x: 0, y: show ? -200 : -20)
             .offset(x: viewState.width, y: viewState.height)
-            .scaleEffect(0.95)
+            .offset(y : showCard ? -140 : 0)
+            .scaleEffect(showCard ? 1 : 0.95)
             .rotationEffect(.degrees(show ? 0 : 5))
-            .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+            .rotationEffect(Angle(degrees: showCard ? -5: 0))
+            .rotation3DEffect(Angle(degrees: showCard ? 0 : 5), axis: (x: 10.0, y: 0, z: 0))
             .blendMode(.hardLight)
             .animation(.easeIn(duration: 0.3))
             
             
             CardView()
+                .frame(width: showCard ? 375 : 340.0, height: 220.0)
+                .background(Color.black)
+//                .cornerRadius(20)
+            
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+            
+                .shadow(radius: 20)
                 .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -100 : 0)
                 .blendMode(.hardLight)
                 .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
@@ -71,16 +98,53 @@ struct ContentView: View {
                             
                         }
                 )
-                
+            
+//            Text("\(bottomState.height)").offset(y: -300)
             
             
             BottomCardView()
                 .offset(x: 0, y: showCard ? 360 : 1000)
+                .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
             
 //            Used the cubic-bezier website to generate my custom animation timingCurve
             
                 .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+
+//            Implimenting gesture controls
+            
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.bottomState = value.translation
+                        
+                        if self.showFull{
+                            self.bottomState.height += -300
+                        }
+                        if self.bottomState.height < -300{
+                            self.bottomState.height = -300
+                        }
+                        
+                    }
+                        .onEnded {
+                            value in
+                            if self.bottomState.height > 50{
+                                self.showCard = false
+                            }
+                            
+                            if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull){
+                                self.bottomState.height = -300
+                                self.showFull = true
+                            }
+                            else{
+                                self.bottomState = .zero
+                                self.showFull = false
+                            }
+                            
+                            
+                        }
+                )
+            
+            
         }
     }
 }
@@ -124,10 +188,7 @@ struct CardView: View {
             
             
         }
-        .frame(width: 340.0, height: 220.0)
-        .background(Color.black)
-        .cornerRadius(20)
-        .shadow(radius: 20)
+       
     }
 }
 
@@ -136,7 +197,7 @@ struct BackCardView: View {
         VStack {
             Spacer()
         }
-        .frame(width: 340, height: 220)
+        
         
     }
 }
