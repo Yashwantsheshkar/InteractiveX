b03dfd501a02627b257768757f08d6389624dbbd
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-29 18:52:41 +0530

    Dynamic Card details linked with an array of values i decide

--- InteractiveX/HomeView.swift
+++ InteractiveX/HomeView.swift
@@ -26,8 +26,8 @@ struct HomeView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30.0) {
-                    ForEach(0 ..< 5) { item in
-                        SectionView()
+                    ForEach(sectionView) { item in
+                        SectionView(section: item)
                     }
                     
                 }
@@ -48,30 +48,53 @@ struct HomeView: View {
 }
 
 struct SectionView: View {
+    
+    var section : Section
+    
     var body: some View {
         VStack {
             HStack {
-                Text("Prototype design in SwiftUI")
+                Text(section.title)
                     .font(.system(size: 24) .bold())
                     .foregroundColor(Color.white)
                     .frame(width: 160, alignment: .leading)
                 
                 Spacer()
                 
-                Image("Logo1")
+                Image(section.logo)
                 
             }
-            Text("18 sections" .uppercased())
+            Text(section.text.uppercased())
                 .frame(maxWidth: .infinity, alignment: .leading)
-            Image("Card1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
         }
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
     }
 }
+
+
+
+struct Section : Identifiable {
+    var id = UUID()
+    var title : String
+    var text : String
+    var logo : String
+    var image : Image
+    var color : Color
+    
+}
+
+let sectionView = [
+Section(title: "Prototype design in SwiftUI", text: "18 sections", logo: "Logo1", image: Image("Card1"), color: Color("card1")),
+Section(title: "Build a SwiftUI app", text: "20 sections", logo: "Logo1", image: Image("Card2"), color: Color("card2")),
+Section(title: "SwiftUI Advanced", text: "28 sections", logo: "Logo1", image: Image("Card3"), color: Color("card3"))
+]
+
+
